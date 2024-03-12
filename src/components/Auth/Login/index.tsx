ebd8e1fbdea41c3857dc41e8a3505b2fd799c40c
유지민 <a01040987587@gmail.com>
2024-03-12 10:28:36 +0900

    commit 누락

--- src/components/Auth/Login/index.tsx
+++ src/components/Auth/Login/index.tsx
@@ -31,48 +31,44 @@ const Login = () => {
   };
 
   return (
-    <l.LoginContainer>
-      <l.LoginWrapper onSubmit={handleLogin}>
-        <l.LoginTopWrap>
-          <img src={Logo} width={50} height={50} alt="logo" />
-          <l.LoginHeading>환영합니다</l.LoginHeading>
-          <l.LoginSubHeading>
-            가장 트렌디한 학생 커뮤니티, 레츠고입니다
-          </l.LoginSubHeading>
-        </l.LoginTopWrap>
-        <l.InputContainer>
-          <l.LoginInput
-            type="text"
-            placeholder="아이디를 입력해주세요"
-            name="userId"
-            value={loginData.userId}
-            onChange={handleInputChange}
-          />
-          <l.LoginInput
-            type="password"
-            placeholder="비밀번호를 입력해주세요"
-            name="password"
-            value={loginData.password}
-            onChange={handleInputChange}
-          />
-        </l.InputContainer>
-        <l.LoginButton type="submit">로그인</l.LoginButton>
-        <l.LoginOppositePartWrap
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <l.LoginOppositePartText>
-            아직 레츠고 회원이 아니신가요?
-          </l.LoginOppositePartText>
-          <l.LoginOppositePartButton onClick={() => navigate("Signup")}>
-            회원가입
-          </l.LoginOppositePartButton>
-        </l.LoginOppositePartWrap>
-      </l.LoginWrapper>
-    </l.LoginContainer>
+    <>
+      <l.LoginContainer>
+        <l.LoginWrapper onSubmit={handleLogin}>
+          <l.LoginTopWrap>
+            <img src={Logo} width={50} height={50} alt="logo" />
+            <l.LoginHeading>환영합니다</l.LoginHeading>
+            <l.LoginSubHeading>
+              가장 트렌디한 학생 커뮤니티, 레츠고입니다
+            </l.LoginSubHeading>
+          </l.LoginTopWrap>
+          <l.InputContainer>
+            <l.LoginInput
+              type="text"
+              placeholder="아이디를 입력해주세요"
+              name="userId"
+              value={loginData.userId}
+              onChange={handleInputChange}
+            />
+            <l.LoginInput
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              name="password"
+              value={loginData.password}
+              onChange={handleInputChange}
+            />
+          </l.InputContainer>
+          <l.LoginButton type="submit">로그인</l.LoginButton>
+          <l.LoginOppositePartWrap>
+            <l.LoginOppositePartText>
+              아직 레츠고 회원이 아니신가요?
+            </l.LoginOppositePartText>
+            <l.LoginOppositePartButton onClick={() => navigate("/Signup")}>
+              회원가입
+            </l.LoginOppositePartButton>
+          </l.LoginOppositePartWrap>
+        </l.LoginWrapper>
+      </l.LoginContainer>
+    </>
   );
 };
 

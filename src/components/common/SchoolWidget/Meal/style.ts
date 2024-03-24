330099b37b94b2884cf8eaff81d75e77d47e0653
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2024-03-24 18:40:59 +0900

    feat : 급식 위젯 뷰 개발

---
+++ src/components/common/SchoolWidget/Meal/style.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+export const MealWidgetWraper = styled.div`
+  width: 290px;
+  height: 100px;
+`;
+
+export const MealWidgetimage = styled.img`
+  position: absolute;
+  width: 290px;
+  height: 100px;
+`;
+
+export const MealWidgetContents = styled.div`
+  position: relative;
+  width: 290px;
+  height: 100px;
+  padding: 5%;
+`;
+
+export const MealWidgetTitle = styled.h2`
+  font-family: "pretendard";
+  font-weight: 500;
+  font-size: 1em;
+  color: white;
+  margin-bottom: 3%;
+`;
+
+export const MealWidgetMenu = styled.h2`
+  width: 200px;
+  font-family: "pretendard";
+  font-weight: 400;
+  font-size: 0.8em;
+  color: white;
+`;
--- src/components/common/SchoolWidget/index.tsx
+++ src/components/common/SchoolWidget/index.tsx
@@ -1,9 +1,11 @@
+import MealWidget from "./Meal";
 import * as S from "./style";
 const SchoolWidget = () => {
   return (
     <>
       <S.SchoolWidgetContainer>
         <S.SchoolWidgetTitle>지금 우리 학교는?</S.SchoolWidgetTitle>
+        <MealWidget />
       </S.SchoolWidgetContainer>
     </>
   );

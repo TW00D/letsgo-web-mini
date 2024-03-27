53ce970527805620ab2bedfc61a8e633f1ebcbfd
유지민 <a01040987587@gmail.com>
2024-03-27 21:42:06 +0900

    fix: 오류 수정

--- src/components/common/Table/Detail/index.tsx
+++ src/components/common/Table/Detail/index.tsx
@@ -1,22 +1,31 @@
-import Nav from "../../Nav"
+import Nav from "../../Nav";
 import Banner from "../../../../assets/banner.svg";
-import * as d from "./style"
+import * as d from "./style";
 import { Header } from "../../Header";
 
 const PostDetail = () => {
-    return (
-        <d.DetailContainer>
-            <Nav />
-            <d.DetailTopWrap>
-              <img src={Banner} alt="배너입니다." />
-              <Header />
-            </d.DetailTopWrap>
-            <d.PostWrap>
-                <d.PostWrapContainer>
-                </d.PostWrapContainer>
-            </d.PostWrap>
-        </d.DetailContainer>
-    )
-}
+  return (
+    <d.DetailContainer>
+      <Nav />
+      <d.DetailTopWrap>
+        <img src={Banner} alt="배너입니다." />
+        <Header />
+      </d.DetailTopWrap>
+      <d.PostWrap>
+        <d.PostTopWrap>
+          <d.PostTitle>홍대에서 페이커 만남 ㄹㅇ.jpg</d.PostTitle>
+          <d.PostBottomTitleWrap>
+            <d.PostLeftTitle>게임</d.PostLeftTitle>
+            <d.PostLeftTitle>1일진</d.PostLeftTitle>
+            <d.PostLeftTitle>대구소프트웨어마이스터고 3학년 여학생</d.PostLeftTitle>
+            <d.PostRightTitle>조회수</d.PostRightTitle>
+            <d.PostRightTitle>댓글</d.PostRightTitle>
+            <d.PostRightTitle>좋아요</d.PostRightTitle>
+          </d.PostBottomTitleWrap>
+        </d.PostTopWrap>
+      </d.PostWrap>
+    </d.DetailContainer>
+  );
+};
 
 export default PostDetail;
--- src/components/common/Table/Detail/style.ts
+++ src/components/common/Table/Detail/style.ts
@@ -22,13 +22,48 @@ export const DetailTopWrap = styled.div`
 
 export const PostWrap = styled.div`
   width: 40%;
-  height: 150%;
+  height: 100%;
   background-color: #fff;
   margin-top: 2%;
+  padding-left: 1%;
 
   overflow: auto;
 `;
 
-export const PostWrapContainer = styled.div`
-  height: 100%;
+export const PostBottomTitleWrap = styled.div`
+  display: flex;
+  margin-top: 2%;
+`
+
+export const PostTopWrap = styled.div`
+  width: 100%;
+  height: 16%;
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid #f1f1f3;
+`;
+
+export const PostTitle = styled.h1`
+  font-size: 24px;
+  font-family: "Pretendard";
+  font-weight: 400;
+  color: #000;
+
+  margin-top: 3%;
+`;
+
+export const PostLeftTitle = styled.h3`
+  font-size: 14px;
+  font-family: "Pretendard";
+  font-weight: 400;
+  color: #706f88;
+  
+  margin-right: 2%;
+`;
+
+export const PostRightTitle = styled.h3`
+  font-size: 14px;
+  font-family: "Pretendard";
+  font-weight: 400;
+  color: #706f88;
 `;
